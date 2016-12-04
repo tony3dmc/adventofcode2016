847a56394d988cbdc346ac9e1fdcd3a6495b3fe0
Tony McNulty <tony@3dmarcomms.com>
2016-12-04 23:02:45 +0000

    Day 3, part 2

---
+++ day3/solution_part2.js
@@ -0,0 +1,30 @@
+var common = require('../common.js');
+
+function validTriangle(sides) {
+  if (sides[0] + sides[1] > sides[2]) {
+    if (sides[1] + sides[2] > sides[0]) {
+      if (sides[2] + sides[0] > sides[1]) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+
+var ints = common.readWords('input.txt').toInts();
+var triangles = [];
+for (var i = 0; i < ints.length; i += 9) {
+  triangles.push([ints[i], ints[i + 3], ints[i + 6]]);
+  triangles.push([ints[i + 1], ints[i + 4], ints[i + 7]]);
+  triangles.push([ints[i + 2], ints[i + 5], ints[i + 8]]);
+}
+
+var counter = 0;
+triangles.forEach(function(sides) {
+  if (validTriangle(sides)) {
+    counter++;
+  }
+});
+
+console.log("There were " + counter + " valid triangles supplied");
